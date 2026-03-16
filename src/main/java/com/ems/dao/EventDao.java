3351846c3d9955a1bc965c85438c9559fe117ed9
Mythilysenthil <mythilsenthilp@gamil.com>
2026-03-16 21:33:59 +0530

    Worked on Registration history

--- src/main/java/com/ems/dao/EventDao.java
+++ src/main/java/com/ems/dao/EventDao.java
@@ -4,33 +4,38 @@ import java.util.List;
 import com.ems.exception.DataAccessException;
 import com.ems.model.Event;
 import com.ems.model.Ticket;
+import com.ems.model.BookingDetail;
 
 public interface EventDao {
 
 	/**
 	 * Lists all published future events that still have available tickets
-	 *
-	 * @return list of available events
-	 * @throws DataAccessException
 	 */
 	List<Event> listAvailableEvents() throws DataAccessException;
 
 	/**
 	 * Retrieves ticket options for a specific event
-	 *
-	 * @param eventId
-	 * @return list of ticket options
-	 * @throws DataAccessException
 	 */
 	List<Ticket> getTicketsByEventId(int eventId) throws DataAccessException;
 
 	/**
 	 * Retrieves full details of a specific event
-	 *
-	 * @param eventId
-	 * @return Event object
-	 * @throws DataAccessException
 	 */
 	Event getEventById(int eventId) throws DataAccessException;
+
+	/**
+	 * View upcoming events registered by a user
+	 */
+	List<Event> getUpcomingEventsByUser(int userId) throws DataAccessException;
+
+	/**
+	 * View past events attended by a user
+	 */
+	List<Event> getPastEventsByUser(int userId) throws DataAccessException;
+
+	/**
+	 * View booking details for a specific user
+	 */
+	List<BookingDetail> getBookingHistory(int userId) throws DataAccessException;
 
 }
--- src/main/java/com/ems/model/BookingDetail.java
+++ src/main/java/com/ems/model/BookingDetail.java
@@ -2,6 +2,26 @@ package com.ems.model;
 
 public class BookingDetail {
 
+	public void setBookingId(int int1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setUserId(int int1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEventId(int int1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setTotalAmount(double double1) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
 
 }
